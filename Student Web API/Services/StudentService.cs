ac76bba3c14f29fbc392080b281d11fd997b6e48
Nqobile Moyo <nqobilemoyo43@gmail.com>
2025-07-04 16:34:33 +0200

    Add Async await

--- Student Web API/Services/StudentService.cs
+++ Student Web API/Services/StudentService.cs
@@ -1,4 +1,5 @@
-﻿using Student_Web_API.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Web_API.Data;
 using Student_Web_API.Model;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace Student_Web_API.Services
         }
         public async Task<ICollection<Student>> GetAll()
         {
-            return _context.Students.OrderBy(x => x.Id).ToList();
+            return await _context.Students.OrderBy(x => x.Id).ToListAsync();
         }
 
         public async Task<bool> Create(Student student)
@@ -23,22 +24,22 @@ namespace Student_Web_API.Services
             var studentCount = _context.Students.ToList().Count;
             studentCount++;
             student.Id = studentCount;
-            _context.Students.Add(student);
-            return await Save();
+           await  _context.Students.AddAsync(student);
+            return await SaveAsync();
         }
         public async Task<bool> Exists(int id)
         {
-            return _context.Students.Any(x => x.Id == id);
+            return await _context.Students.AnyAsync(x => x.Id == id);
         }
         public async Task<bool> Delete(int id)
         {
             var student = _context.Students.Where(x => x.Id == id).FirstOrDefault();
             _context.Students.Remove(student);
-            return await Save();
+            return await SaveAsync();
         }
-        public async Task<bool> Save()
+        public async Task<bool> SaveAsync()
         {
-            return _context.SaveChanges() >= 0 ? true : false;
+            return await _context.SaveChangesAsync() >= 0 ? true : false;
         }
     }
 }
